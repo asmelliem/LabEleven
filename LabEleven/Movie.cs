ec31cce0c4d53ee18955e613862525cf4a16380c
Ada <asmelliem@gmail.com>
2020-08-04 15:50:16 -0400

    Made changes to generate list and loop through to display the movies in the category that the user has specified

---
+++ LabEleven/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabEleven
+{
+    public class Movie
+    {
+        private string _title;
+
+        private string _category;
+
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        public string Category
+        {
+            get { return _category; }
+        }
+
+        public Movie(string title, string category)
+        {
+            this._title = title;
+            this._category = category;
+        }
+    }
+}
--- LabEleven/Program.cs
+++ LabEleven/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace LabEleven
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please select a category (1-4): \n" +
+                "1. Animated\n" +
+                "2. Drama\n" +
+                "3. Horror\n" +
+                "4. Scifi");
+
+            var category = SetCategoryChoice(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(ReturnMovieList(GenerateMovieList(), category));          
+        }
+
+        public static List<Movie> GenerateMovieList()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie("Pokemon", "Animated"));
+            movies.Add(new Movie("Frozen", "Animated"));
+            movies.Add(new Movie("Weathering with You", "Animated"));
+            movies.Add(new Movie("Rocketman", "Drama"));
+            movies.Add(new Movie("Little Women", "Drama"));
+            movies.Add(new Movie("IT", "Horror"));
+            movies.Add(new Movie("The Conjuring", "Horror"));
+            movies.Add(new Movie("IT", "Horror"));
+            movies.Add(new Movie("Star Wars", "Scifi"));
+            movies.Add(new Movie("Minority Report", "Scifi"));
+            return movies;
+        }
+
+        public static StringBuilder ReturnMovieList(List<Movie> movies, string category)
+        {
+            StringBuilder movieList = new StringBuilder();
+            foreach(var movie in movies)
+            {
+                if (movie.Category == category)
+                {
+                    movieList.Append($"{movie.Title}\n");
+                }
+            }
+
+            return movieList;
+        }
+
+        public static string SetCategoryChoice(int input)
+        {
+            if (input == 1)
+            {
+                return "Animated";
+            }
+            else if (input == 2)
+            {
+                return "Drama";
+            }
+            else if (input == 3)
+            {
+                return "Horror";
+            }
+            else
+            {
+                return "Scifi";
+            }
         }
     }
 }
